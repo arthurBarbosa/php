010c098ec23bf12a863aa99703e9e196de516f93
Arthur Barbosa <arthurbruno03@gmail.com>
2018-03-27 14:59:43 -0300

    criado a classe usuário

--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,16 @@
 
 require_once ("config.php");
 
-$sql = new Sql();
-
+/*
 $usuarios = $sql->select("select * from tb_usuarios");
 
 echo json_encode($usuarios);
+*/
 
+
+$root = new Usuario();
+
+$root->loadById(3);
+
+echo $root;
  ?>
