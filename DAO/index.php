8494c5604292b6591a25920ba53f27c5addeb1e9
Arthur Barbosa <arthurbruno03@gmail.com>
2018-03-27 16:08:59 -0300

    criado alguns metodos utilitarios de usuário

--- DAO/index.php
+++ DAO/index.php
@@ -9,9 +9,28 @@ echo json_encode($usuarios);
 */
 
 
-$root = new Usuario();
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root ;
 
-$root->loadById(3);
 
-echo $root;
+//carrega uma lista de usuarios
+//$lista = new Usuario();
+//$resultado = $lista->getList();
+
+//echo json_encode($resultado);
+
+//metodo sendo executado atraves de um metodo static->não precisa instancia o objeto, pode ser chamado direto
+//$listaComMetodoStatic = Usuario::getList();
+//echo json_encode($listaComMetodoStatic);
+
+/*$search = Usuario::search("ar");
+echo json_encode($search);*/
+
+//carrega um usuario usando o login e a senha 
+$usuario = new Usuario();
+$usuario->login("jose","12345");
+
+echo $usuario;
+
  ?>
